acf7399ba166e083687c4041db63b3e576098231
Pat Allan <pat@freelancing-gods.com>
2017-02-22 22:46:34 +1100

    Basic maintenance page.

---
+++ config/puma.rb
@@ -0,0 +1,43 @@
+HTML_BODY = <<-HTML
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Under Maintenance</title>
+</head>
+<body>
+  <h1>Under Maintenance</h1>
+  <p>This site is currently undergoing maintenance. We'll have it back online as soon as possible.</p>
+</body>
+</html>
+HTML
+
+JSON_BODY = <<-JSON
+{
+  "status": "Under Maintenance"
+  "message": "This site is currently underoing maintenance. We'll have it back online as soon as possible."
+}
+JSON
+
+tag 'Maintenance'
+
+app do |env|
+  if env.fetch('HTTP_ACCEPT', '')[/json/]
+    [
+      503,
+      {
+        'Content-Type' => 'application/json',
+        'Content-Length' => JSON_BODY.length.to_s
+      },
+      [JSON_BODY]
+    ]
+  else
+    [
+      503,
+      {
+        'Content-Type' => 'text/html',
+        'Content-Length' => HTML_BODY.length.to_s
+      },
+      [HTML_BODY]
+    ]
+  end
+end
